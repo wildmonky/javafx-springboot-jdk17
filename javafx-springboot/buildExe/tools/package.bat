0bbb6b8076554689fc34b2b3ed437a44155a8e6c
wildmonky <43490169+wildmonky@users.noreply.github.com>
2025-08-21 16:02:41 +0800

    javafx + spring 打包配置调整

--- javafx-springboot/buildExe/tools/package.bat
+++ javafx-springboot/buildExe/tools/package.bat
@@ -14,17 +14,17 @@ if not %ERRORLEVEL% EQU 0 (
 
 echo remove existed exe >> %log-file%
 IF EXIST %app-name% rmdir /Q /S %app-name%
-IF not EXIST ..\target\rule-validator-1.0-SNAPSHOT.jar (
+IF not EXIST ..\target\exec\app.jar (
     echo "please use idea to package first" >> %log-file%
     exit /b
 )
 ::pause 暂停
 :: --win-console 运行时是否开启控制台
 %jpackage% --type app-image --name %app-name%  ^
-    --input ..\target\ --main-jar rule-validator-1.0-SNAPSHOT.jar ^
+    --input ..\target\exec\ --main-jar app.jar ^
     --icon ..\src\main\resources\lizi.png ^
     --win-console ^
-    --dest .\ --temp ~/temp/rule-validator
+    --dest .\ --temp .\temp\rule-validator
 
 if exist %app-name% (
     echo build new exe success >> %log-file%
