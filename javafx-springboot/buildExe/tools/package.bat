c9d1240d4dd441d52ab881a347d773724ed64b5f
wildmonky <43490169+wildmonky@users.noreply.github.com>
2025-08-21 17:22:49 +0800

    1、修改pom.xml； 2、完善打包脚本 bat

--- javafx-springboot/buildExe/tools/package.bat
+++ javafx-springboot/buildExe/tools/package.bat
@@ -14,9 +14,13 @@ if not %ERRORLEVEL% EQU 0 (
 
 echo remove existed exe >> %log-file%
 IF EXIST %app-name% rmdir /Q /S %app-name%
-IF not EXIST ..\target\exec\app.jar (
-    echo "please use idea to package first" >> %log-file%
-    exit /b
+IF not EXIST %~dp0..\..\target\exec\app.jar (
+    echo packaging... >> %log-file%
+    call mvn clean -D"maven.test.skip"=true package -f %~dp0..\..\pom.xml
+    IF not EXIST %~dp0..\..\target\exec\app.jar (
+        echo mvn clean -D"maven.test.skip"=true  package: maven package failed >> %log-file%
+        exit /b
+    )
 )
 ::pause 暂停
 :: --win-console 运行时是否开启控制台
